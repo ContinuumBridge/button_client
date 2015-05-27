6a64cf0a85a489bd7d9768c2f97a413a0b8e02a2
PeterClaydon <peter.claydon@continuumbridge.com>
2015-05-27 15:35:59 +0000

    Basic functionality

---
+++ app_client/app_client.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python
+# button_client.py
+# Copyright (C) ContinuumBridge Limited, 2015 - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Peter Claydon
+#
+"""
+Bit of a botch for now. Just stick actions from incoming requests into threads.
+"""
+
+import json
+import requests
+import websocket
+import time
+import sys
+import signal
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.MIMEImage import MIMEImage
+import subprocess
+import logging
+import logging.handlers
+import twilio
+import twilio.rest
+from twisted.internet import threads
+from twisted.internet import reactor, defer
+from subprocess import check_output
+import os.path
+
+config                = {}
+HOME                  = os.path.expanduser("~")
+CONFIG_FILE           = HOME + "/button_client.config"
+CB_ADDRESS            = "portal.continuumbridge.com"
+DBURL                 = "http://onepointtwentyone-horsebrokedown-1.c.influxdb.com:8086/"
+CB_LOGGING_LEVEL      = "DEBUG"
+CB_LOGFILE            = HOME + "/button_client.log"
+TWILIO_ACCOUNT_SID    = "AC72bb42908df845e8a1996fee487215d8" 
+TWILIO_AUTH_TOKEN     = "717534e8d9e704573e65df65f6f08d54"
+TWILIO_PHONE_NUMBER   = "+441183241580"
+CONFIG_READ_INTERVAL  = 600
+ 
+logger = logging.getLogger('Logger')
+logger.setLevel(CB_LOGGING_LEVEL)
+handler = logging.handlers.RotatingFileHandler(CB_LOGFILE, maxBytes=10000000, backupCount=5)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+def nicetime(timeStamp):
+    localtime = time.localtime(timeStamp)
+    milliseconds = '%03d' % int((timeStamp - int(timeStamp)) * 1000)
+    now = time.strftime('%H:%M:%S, %d-%m-%Y', localtime)
+    return now
+
+def sendMail(to, subject, body):
+    try:
+        user = config["mail"]["user"]
+        password = config["mail"]["password"]
+        # Create message container - the correct MIME type is multipart/alternative.
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = subject
+        msg['From'] = config["mail"]["from"]
+        recipients = to.split(',')
+        [p.strip(' ') for p in recipients]
+        if len(recipients) == 1:
+            msg['To'] = to
+        else:
+            msg['To'] = ", ".join(recipients)
+        # Create the body of the message (a plain-text and an HTML version).
+        text = body + " \n"
+        htmlText = text
+        # Record the MIME types of both parts - text/plain and text/html.
+        part1 = MIMEText(text, 'plain')
+        part2 = MIMEText(htmlText, 'html')
+        msg.attach(part1)
+        msg.attach(part2)
+        mail = smtplib.SMTP('smtp.gmail.com', 587)
+        mail.ehlo()
+        mail.starttls()
+        mail.login(user, password)
+        mail.sendmail(user, recipients, msg.as_string())
+        logger.debug("Sent mail")
+        mail.quit()
+    except Exception as ex:
+        logger.warning("sendMail problem. To: %s, type %s, exception: %s", to, type(ex), str(ex.args))
+       
+def postData(dat, bid):
+    try:
+        url = ""
+        for b in config["bridges"]:
+            if b["bid"] == bid:
+                if "database" in b:
+                    url = DBURL + "db/" + b["database"] + "/series?u=root&p=27ff25609da60f2d"
+                else:
+                    url = DBURL + "db/Bridges/series?u=root&p=27ff25609da60f2d"
+                break
+        headers = {'Content-Type': 'application/json'}
+        status = 0
+        logger.debug("url: %s", url)
+        r = requests.post(url, data=json.dumps(dat), headers=headers)
+        status = r.status_code
+        if status !=200:
+            logger.warning("POSTing failed, status: %s", status)
+    except Exception as ex:
+        logger.warning("postData problem, type %s, exception: %s", to, type(ex), str(ex.args))
+
+def sendSMS(messageBody, to):
+    numbers = to.split(",")
+    for n in numbers:
+       try:
+           client = twilio.rest.TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+           message = client.messages.create(
+               body = messageBody,
+               to = n,
+               from_ = TWILIO_PHONE_NUMBER
+           )
+           sid = message.sid
+           logger.debug("Sent sms: %s", bid, str(n))
+       except Exception as ex:
+           logger.warning("sendSMS, unable to send message %s to: %s, type %s, exception: %s", messageBody, str(to), type(ex), str(ex.args))
+
+class Connection(object):
+    def __init__(self):
+        logger.debug("Connection __init__")
+        signal.signal(signal.SIGINT, self.signalHandler)  # For catching SIGINT
+        signal.signal(signal.SIGTERM, self.signalHandler)  # For catching SIGTERM
+        self.stopping = False
+        self.buttonStates = {}
+        self.reconnects = 0
+        self.reauthorise = 0
+        self.sendCount = 0
+        self.readConfig(True)
+        self.configLoop = reactor.callLater(CONFIG_READ_INTERVAL, self.readConfigLoop)
+        reactor.callLater(1, self.authorise)
+        reactor.addSystemEventTrigger('before', 'shutdown', self.tidyUp)
+        reactor.run()
+
+    def signalHandler(self, signal, frame):
+        logger.debug("signalHandler received signal")
+        self.stopping = True
+        reactor.stop()
+        time.sleep(0.5)
+        sys.exit(1)
+
+    def tidyUp(self):
+        self.configLoop.cancel()
+
+    def readConfig(self, forceRead=False):
+        logger.debug("Reading config")
+        try:
+            if time.time() - os.path.getmtime(CONFIG_FILE) < 600 or forceRead:
+                global config
+                with open(CONFIG_FILE, 'r') as f:
+                    newConfig = json.load(f)
+                    logger.info( "Read button_client.config")
+                    config.update(newConfig)
+                    logger.info("Config read")
+                for c in config:
+                    if c.lower in ("true", "t", "1"):
+                        config[c] = True
+                    elif c.lower in ("false", "f", "0"):
+                        config[c] = False
+                logger.info("Read new config: " + json.dumps(config, indent=4))
+        except Exception as ex:
+            logger.warning("Problem reading button_client.config, type: %s, exception: %s", str(type(ex)), str(ex.args))
+
+    def readConfigLoop(self):
+        logger.debug("readConfigLoop")
+        reactor.callLater(10, self.readConfig())
+        self.configLoop = reactor.callLater(CONFIG_READ_INTERVAL, self.readConfigLoop)
+
+    def authorise(self):
+        try:
+            self.reconnects = 0
+            auth_url = "http://" + CB_ADDRESS + "/api/client/v1/client_auth/login/"
+            auth_data = '{"key": "' + config["cid_key"] + '"}'
+            auth_headers = {'content-type': 'application/json'}
+            response = requests.post(auth_url, data=auth_data, headers=auth_headers)
+            self.cbid = json.loads(response.text)['cbid']
+            self.sessionID = response.cookies['sessionid']
+            self.ws_url = "ws://" + CB_ADDRESS + ":7522/"
+            reactor.callLater(0.1, self.connect)
+        except Exception as ex:
+            logger.warning("Unable to authorise with server, type: %s, exception: %s", str(type(ex)), str(ex.args))
+
+    def connect(self):
+        try:
+            websocket.enableTrace(True)
+            self.ws = websocket.WebSocketApp(
+                            self.ws_url,
+                            on_open   = self.onopen,
+                            on_error = self.onerror,
+                            on_close = self.onclose,
+                            header = ['sessionID: {0}'.format(self.sessionID)],
+                            on_message = self.onmessage)
+            self.ws.run_forever()
+        except Exception as ex:
+            self.reconnects += 1
+            logger.warning("Websocket connection failed, type: %s, exception: %s", str(type(ex)), str(ex.args))
+
+    def onopen(self, ws):
+        self.reconnects = 0
+        logger.debug("on_open")
+
+    def onclose(self, ws):
+        if not self.stopping:
+            if self.reconnects < 4:
+                logger.debug("on_close. Attempting to reconnect.")
+                reactor.callLater((self.reconnects+1)*5, self.connect)
+            else:
+                logger.error("Max number of reconnect tries exceeded. Reauthenticating.")
+                reactor.callLater(5, self.authorise)
+
+    def onerror(self, ws, error):
+        logger.error("Error: %s", str(error))
+
+    def onmessage(self, ws, message):
+        try:
+            msg = json.loads(message)
+            logger.info("Message received: %s", json.dumps(msg, indent=4))
+        except Exception as ex:
+            logger.warning("onmessage. Unable to load json, type: %s, exception: %s", str(type(ex)), str(ex.args))
+        if not "body" in msg:
+            logger.warning("button_client. onmessage. message without body")
+            return
+        if msg["body"] == "connected":
+            logger.info("Connected to ContinuumBridge")
+        else:
+            try:
+                bid = msg["source"].split("/")[0]
+                if bid not in config["bridges"]:
+                    logger.info("button_client. Message received from unregistered bridge: %s", bid)
+                    return
+                found = False
+                rx_n = 0
+                for body in msg["body"]:
+                    logger.debug("body: %s", str(body))
+                    if "n" in body:
+                        if rx_n <= body["n"]:
+                            rx_n = body["n"]
+                            found = True
+                            del body["n"]
+                    if "a" not in body:  # "a" is a message ack
+                        logger.info("onMessage to process: %s", str(json.dumps(body, indent=4)))
+                        for b in config["buttons"]:
+                            if body["b"] == b["id"]:
+                                changed = False
+                                if "s" in body:
+                                    if body["s"] == 1:
+                                        alert = b["name"] + " activated"
+                                    elif body["s"] == 0:
+                                        alert = b["name"] + " cleared"
+                                    subject =  alert
+                                    if "email" in b:
+                                        reactor.callInThread(sendMail, b["email"], subject, alert)
+                                    if "sms" in b:
+                                        reactor.callInThread(sendSMS, alert, b["sms"])
+                                    changed = True
+                                if "c" in body:
+                                    if b["id"] in self.buttonStates:
+                                        if body["c"] != self.buttonStates[b["id"]]["connected"]:
+                                            self.buttonStates[b["id"]]["connected"] = body["c"]
+                                            changed = True
+                                    else:
+                                        self.buttonStates[b["id"]] = {
+                                            "connected": body["c"],
+                                            "bid": bid,
+                                            "ssid": 0
+                                        }
+                                        changed = True
+                                if "p" in body:
+                                    if bid == self.buttonStates[b["id"]]["bid"]:
+                                        self.buttonStates[b["id"]]["ssid"] = body["p"]
+                                        changed = True
+                                    elif body["p"] > self.buttonStates[b["id"]]["ssid"]:
+                                        self.buttonStates[b["id"]]["ssid"] = body["p"]
+                                        self.buttonStates[b["id"]]["bid"] = bid
+                                        changed = True
+                                if changed:
+                                    logger.debug("State changed: %s", str(json.dumps(self.buttonStates, indent=4)))
+                                break
+                if found:
+                    ack = {
+                            "source": config["cid"],
+                            "destination": msg["source"],
+                            "body": [
+                                        {"a": rx_n}
+                                    ]
+                          }
+                    logger.debug("onMessage ack: %s", str(json.dumps(ack, indent=4)))
+                    self.ws.send(json.dumps(ack))
+            except Exception as ex:
+                logger.warning("onmessage. Problem processing message, type: %s, exception: %s", str(type(ex)), str(ex.args))
+
+if __name__ == '__main__':
+    connection = Connection()
