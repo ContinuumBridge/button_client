9b5d710093f16d20c14da162d8373f4d5a6e74dd
Mark Claydon <developer@glia.co>
2015-10-28 16:35:23 +0000

    Collections subscribing successfully, button enabled checkbox not working

--- server/publish.js
+++ server/publish.js
@@ -1,48 +1,78 @@
 
 Meteor.publish('users', function() {
 
-  if (Roles.userIsInRole(this.userId, ['admin'])) {
-      return Users.find({});
-  } else {
-      return Users.find({_id: this.userId});
-  }
+    if (this.userId) {
+
+        if (Roles.userIsInRole(this.userId, ['admin'])) {
+            return Users.find({});
+        } else {
+            return Users.find({_id: this.userId});
+        }
+    } else {
+
+        this.ready();
+    }
 });
 
 Meteor.publish('organisations', function() {
 
-  if (Roles.userIsInRole(this.userId, ['admin'])) {
-      return Organisations.find({});
-  } else {
-      var user = Users.build(Users.findOne(this.userId));
-      //console.log('user.organisations.find()', user.organisations.find());
-      return user.organisations.find();
-      //return Organisations.find({_id: this.userId});
-  }
+    if (this.userId) {
+
+        if (Roles.userIsInRole(this.userId, ['admin'])) {
+            return Organisations.find({});
+        } else {
+            var user = Users.build(Users.findOne(this.userId));
+            //console.log('user.organisations.find()', user.organisations.find());
+            return user.organisations.find();
+            //return Organisations.find({_id: this.userId});
+        }
+    } else {
+
+        this.ready();
+    }
 });
 
 Meteor.publish('lists', function() {
 
-    var user = Users.build(Users.findOne(this.userId));
-    var organisation = user.organisations.findOne();
-    if (Roles.userIsInRole(this.userId, ['admin'])) {
-        return Lists.find({});
+    //console.log('publish lists this.userId', this.userId);
+    if (this.userId) {
+        var user = Users.build(Users.findOne(this.userId));
+        //if (!user) return;
+        var organisation = user.organisations.findOne();
+        if (Roles.userIsInRole(this.userId, ['admin'])) {
+            return Lists.find({});
+        } else {
+            return Lists.find({organisationId: organisation._id});
+        }
     } else {
-        return Lists.find({organisationId: organisation._id});
+
+        this.ready();
     }
     //return Lists.find({});
 });
 
-Meteor.publish('buttons', function() {
+Meteor.publish('buttons', function(listId) {
 
-    var user = Users.build(Users.findOne(this.userId));
-    var organisation = user.organisations.findOne();
-    var lists = Lists.find({organisationId: organisation._id}).fetch();
-    var listIds = _.pluck(lists, '_id');
+    console.log('publish buttons', listId);
+    if (this.userId) {
 
-    if (Roles.userIsInRole(this.userId, ['admin'])) {
-        return Buttons.find({});
+        var user = Users.build(Users.findOne(this.userId));
+        var organisation = user.organisations.findOne();
+        var lists = Lists.find({organisationId: organisation._id}).fetch();
+        var listIds = _.pluck(lists, '_id');
+        var index = listIds.indexOf(listId);
+
+        if (index >= 0) {
+
+            return Buttons.find({listId: listId});
+        //} else if (Roles.userIsInRole(this.userId, ['admin'])) {
+
+        } else {
+            this.ready();
+        }
     } else {
-        return Buttons.find({listId: {$in: listIds}});
+
+        this.ready();
     }
 });
 
